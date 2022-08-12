17f1ad5e6e7766ca6a77a9f2cc8421c823f20bfc
aldopaganijr <aldojpagani@gmail.com>
2022-08-12 10:45:43 -0400

    main routes set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.scss"
+import {Routes, Route} from "react-router-dom"
+import Home from "./Routes/Home/Home"
+import Navigation from "./Routes/Navigation/Navigation"
+import Mens from "./Routes/Mens/Mens"
+import Women from "./Routes/Women/Women"
+import NewArrivals from "./Routes/NewArrivals/NewArrivals"
+import Kids from "./Routes/Kids/Kids"
+import Sale from "./Routes/Sale/Sale"
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+          <Route path="/" element={<Navigation />} >
+            <Route index element={<Home />} />
+            <Route path="/NewArrivals" element={<NewArrivals />} />
+            <Route path="/Mens" element={<Mens />} />
+            <Route path="/Women" element={<Women />} />
+            <Route path="/Kids" element={<Kids />} />
+            <Route path="/Sale" element={<Sale />} />
+          </Route>
+      </Routes>
     </div>
   );
 }
--- src/Routes/Navigation/Navigation.js
+++ src/Routes/Navigation/Navigation.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import "./Navigation.scss"
+import { Outlet, Link } from 'react-router-dom'
 
 const Navigation = () => {
 
   return (
-
-    <div>
-        This is Navigation
-    </div>
-
+    <Fragment>
+      <div className='navigation-conatiner'>
+          <Link className='links' to="/">
+            Home
+          </Link>
+          <Link className='links' to="/NewArrivals">
+            New Arrivals
+          </Link>
+          <Link className='links' to="/Mens">
+            Mens
+          </Link>
+          <Link className='links' to="/Women">
+            Womens
+          </Link>
+          <Link className='links' to="/Kids">
+            Kids
+          </Link>
+          <Link className='links' to="/Sale">
+            Sale
+          </Link>
+      </div>
+      <Outlet />
+    </Fragment>
   )
 }
 
