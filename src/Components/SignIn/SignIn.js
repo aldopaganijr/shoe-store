4b6c61c31a4f2ecf2a64057fed1161b0075148fb
aldopaganijr <aldojpagani@gmail.com>
2022-08-15 17:10:00 -0400

    sign in form functionality

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,14 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./SignIn.scss"
-import {signInWithGooglePopup, createUserDocumentFromAuth} from "../../Utils/Firebase";
+import {signInWithGooglePopup} from "../../Utils/Firebase";
 
+// default form fields object for sign in page
+const defaultFormFields = {
+  email: '',
+  password: '',
+}
 
 const SignIn = () => {
 
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
-    console.log(userDocRef)
+  // use state for setting form fields to start at defualt values and changed.
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  // function to clear form fields after sign in.
+  const clearFormField = () => {
+    setFormFields(defaultFormFields)
+  }
+
+  // function to sign in with google popup from firebase.js
+  const signInWithGoogle = async () => {
+    await signInWithGooglePopup();
+  }
+
+  // after form is submitted this function will dictate whats next.
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // This try block will execute the sign in using the function from firebase.js
+    try {
+      const {user} = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      clearFormField();
+
+    // this error block will run two different error responses then run the generic console.log
+    } catch (error){
+      switch(error.code){
+        case "auth/wrong-password":
+          alert('Incorrect password, please try again!');
+          break;
+        case "auth/user-not-found":
+          alert('No user associated with this email');
+         break;
+      default:
+      console.log(error)
+
+   }
+  }
+}
+  // This handle change function will actually set form inputs to state.
+  const handleChange = (event) => {
+    const {name, value} = event.target
+
+    setFormFields({...formFields, [name]: value})
   }
 
   
@@ -18,13 +66,13 @@ const SignIn = () => {
 
     <div className='signincontainer'>
       <h3 className='signinheader'>Already a member? Sign in below</h3>
-      <form className='signinform'>
+      <form onSubmit={handleSubmit} className='signinform'>
         <label>Email</label>
-        <input className='signininputs' />
+        <input className='signininputs' onChange={handleChange} name="email" value={email}/>
         <label>Password</label>
-        <input className='signininputs' />
+        <input className='signininputs' onChange={handleChange} name="password" value={password} />
         <button className='signinbutton'>Sign In</button>
-        <button className='signingoogle' onClick={logGoogleUser}>Sign in with Google</button>
+        <button className='signingoogle' onClick={signInWithGoogle}>Sign in with Google</button>
       </form>
     </div>
 
